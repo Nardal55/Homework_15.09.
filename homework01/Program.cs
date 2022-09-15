7bea12fc632246697f2cba3f331caea6e0b0a8c6
Stanislav <rockman2009@mail.ru>
2022-09-15 14:46:37 +0300

    add work01

--- homework01/Program.cs
+++ homework01/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+Console.Write("Введите число a: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int max = number;
+if (number1 > max) max = number1;
+Console.WriteLine(max);
+
+
+
