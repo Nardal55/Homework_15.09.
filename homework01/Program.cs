da93a4eab2c3977cba037a8c246da0f17b1b61b9
Stanislav <rockman2009@mail.ru>
2022-09-15 15:07:24 +0300

    add new strongs

--- homework01/Program.cs
+++ homework01/Program.cs
@@ -6,8 +6,11 @@ int number = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число b: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int max = number;
+int min = number;
 if (number1 > max) max = number1;
-Console.WriteLine(max);
+if (number1 < min) min = number1;
+Console.Write("Максимальное значение: ");Console.WriteLine(max);
+Console.Write("Минимальное значение: ");Console.WriteLine(min);
 
 
 
