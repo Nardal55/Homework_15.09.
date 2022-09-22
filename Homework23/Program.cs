ee65d2a621669690eb4b048080a5416fba603b65
Stanislav <rockman2009@mail.ru>
2022-09-22 15:44:01 +0300

    add homework23 22.09

---
+++ Homework23/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void cube(int number)
+{
+    int count = 1;
+    while (count <= number)
+    {
+        Console.WriteLine($"|{count, 2} | {count * count * count, 3} |");
+        count++;
+    }
+
+}
+if (num > 0) cube(num);
+else Console.WriteLine("Введен неправильное число");
