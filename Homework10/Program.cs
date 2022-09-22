5f168ac86acb6a4d0b2639b7c737c784d9f5ea5a
Stanislav <rockman2009@mail.ru>
2022-09-22 14:37:49 +0300

    add Homework 19.09

---
+++ Homework10/Program.cs
@@ -0,0 +1,21 @@
+﻿// int number = new Random().Next(100, 1000);
+// Console.WriteLine($"Случайное число  -> {number} ");
+// int secondDigit = number / 10;
+// int result = secondDigit % 10;
+// Console.WriteLine(result);
+
+
+int secondDigit(int num)
+{
+    int SecondDigit = num / 10;
+    int result = SecondDigit % 10;
+    return SecondDigit % 10;
+
+}
+
+int number = new Random().Next(100, 1000);
+int result = secondDigit(number);
+
+Console.WriteLine($"Случайное число  -> {number} ");
+
+Console.WriteLine(result);
