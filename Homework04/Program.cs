97b40fd13794061bfbfacaa90766ba0697f6ecd8
Stanislav <rockman2009@mail.ru>
2022-09-30 09:34:43 +0300

    add homework27 26.09

---
+++ Homework04/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+if (count == 1)
+count += 1;
+while(count <= number)
+Console.WriteLine("Четные числа: " + count);
+
---
+++ homework27 26.09/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumAllNumber(int num)
+{
+    int counter = num;
+    int arg = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+       arg = num - num % 10;
+       result = result + (num - arg);
+       num = num / 10; 
+    }
+    return result;
+}
+int sumAllNumber = SumAllNumber(number);
+Console.WriteLine($"Сумма цифр равна: {sumAllNumber}");
+
