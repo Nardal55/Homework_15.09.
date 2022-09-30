23328dc5cae018a705b41e2d0b6e3970c7ce4e05
Stanislav <rockman2009@mail.ru>
2022-09-30 09:59:02 +0300

    add hw 25 26.09

---
+++ homework25 26.09/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число А: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int Degree(int number1, int number2)
+{
+    int result = 1;
+    for (int i = 1; i <= number2; i++)
+    {
+        result = result * number1;
+    }
+    return result;
+}
+
+int degree = Degree(number1, number2);
+Console.WriteLine($"Результат: {degree}");
