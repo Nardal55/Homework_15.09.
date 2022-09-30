f98f27e14775cef044e329ca46b4163b4058408b
Stanislav <rockman2009@mail.ru>
2022-09-30 09:41:26 +0300

    add hw 29 26.09

---
+++ homework29 26.09/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] array = new int[8];
+GetArray(array);
+PrintArray(array);
+
+
+void GetArray(int[] arr)
+{
+    int length = arr.Length;
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(1, 100);
+    }
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
