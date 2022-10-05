cea1e2bb78e54e06bf7ec4af7607c52fa9c466a4
Stanislav <rockman2009@mail.ru>
2022-10-05 11:32:27 +0300

    Add hw 29.09

---
+++ homework34 29.09/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] CreateArrrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+int EvenNumbers (int[]array)
+{
+    int even = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        even++;
+    }
+    return even;
+}
+
+
+int[] arr = CreateArrrayRndInt(4, 100 ,1000);
+PrintArray(arr);
+int even = EvenNumbers(arr);
+Console.WriteLine($"Четных чисел: -> {even}");
---
+++ homework36 29.09/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] CreateArrrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+int SumOddArray (int[]array)
+{
+    int sumOdd = 0;
+    for (int i = 0; i < array.Length; i = i + 2)
+    {
+        sumOdd = sumOdd + array[i];
+    }
+    return sumOdd;
+}
+
+
+int[] arr = CreateArrrayRndInt(5, -10 , 10);
+PrintArray(arr);
+int sumOdd = SumOddArray(arr);
+Console.WriteLine($"Сумма чисел нечетных элементов: -> {sumOdd}");
+
