7fc9db90abc4ef703b2361502147f530b7e43e1e
Stanislav <rockman2009@mail.ru>
2022-10-09 12:10:26 +0300

    add hw 1.10

---
+++ homework41 1.10/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write($"Введи количество чисел: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int[] aboveZeroNumbers = new int[numbers];
+
+void EntryNumbers(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        aboveZeroNumbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+int CountNumbersMoreZero(int[] aboveZeroNumbers)
+
+{
+    int count = 0;
+    for (int i = 0; i < aboveZeroNumbers.Length; i++)
+    {
+        if (aboveZeroNumbers[i] > 0) count += 1;
+    }
+    return count;
+}
+
+EntryNumbers(numbers);
+int countNumbersMoreZero = CountNumbersMoreZero(aboveZeroNumbers);
+Console.WriteLine($"Чисел больше нуля(0) -> {countNumbersMoreZero}");
+
---
+++ homework43 1.10/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Значение k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Значение k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1) / (-k1 + k2);
+double y = (k2 * x + b2);
+
+Console.WriteLine($"Две прямые пересекаются в точке X: ({x}), Y: ({y})");
+
